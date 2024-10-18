d5d195d786fa218aef46eebfc0faee9bc15fc9ab
parduccinward <dpardoblacutt@gmail.com>
2024-10-18 17:48:47 -0400

    [NOM-1] use prompt layout

--- src/app/components/PromptLayout/PromptLayout.tsx
+++ src/app/components/PromptLayout/PromptLayout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import TextInput from "../TextInput/TextInput";
 import { Switch, Box } from "@mui/material";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import React from "react";
+import PromptLayout from "./components/PromptLayout/PromptLayout";
+import { Typography } from "@mui/material";
+
+import {
+  _PROMPT_DEFAULT_SUGGESTION,
+  _PROMPT_WITH_CITY_PERSONAL_PREFERENCES,
+} from "./constants";
+
+const promptValues = {
+  defaultValue: _PROMPT_DEFAULT_SUGGESTION,
+  valueWhenSwitchClicked: _PROMPT_WITH_CITY_PERSONAL_PREFERENCES,
+};
+
 export default function Home() {
-  return <h1>1,2,3 probando</h1>;
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Discover Your Ideal City as a Digital Nomad with Nomadify
+      </Typography>
+      <PromptLayout promptValues={promptValues} />
+    </div>
+  );
 }
